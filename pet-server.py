b9cde353fe2e484079f31a4cdc539cab72c79f9c
William Krug <william.p.krug@gmail.com>
2021-03-31 14:18:17 -0500

    POST to "owners" table

--- pet-server.py
+++ pet-server.py
@@ -10,5 +10,44 @@ app.config["DEBUG"] = True
 def homePage():
   return "<h1>Welcome to the Pet Hotel<h1>"
 
+@app.route('/owners', methods=['GET', 'POST'])
+def api_owners():
+  if request.method == 'POST':
+    name = request.json['body']['name']
+    try:
+      connection = psycopg2.connect(
+      host="127.0.0.1",
+      port="5432",
+      database="pet_hotel"
+      )
+      cursor = connection.cursor(cursor_factory=RealDictCursor)
+      sqlQuery = 'INSERT INTO "owners" ("name") VALUES (%s)'
+      cursor.execute(sqlQuery, (name,))
+      connection.commit()
+      count = cursor.rowcount
+      print(count, "Owner INSERTED")
+      result = {'status': 'CREATED'}
+      return make_response(jsonify(result), 201)
+    except(Exception, psycopg2.Error) as error:
+      if(connection):
+        print("Failed to insert owner", error)
+        result = {'status': 'ERROR'}
+        return make_response(jsonify(result), 500)
+    finally:
+      if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
+  else:
+    connection = psycopg2.connect(
+      host="127.0.0.1",
+      port="5432",
+      database="pet_hotel"
+    )
+    cursor = connection.cursor()
+    sqlQuery = 'SELECT * FROM "owners"'
+    cursor.execute(sqlQuery)
+    data = cursor.fetchall()
+    return jsonify(data)
 
 app.run()
